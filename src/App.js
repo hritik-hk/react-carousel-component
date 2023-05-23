a2fa711ce977a5be843034040fd14e0ab6f0ea3a
hritik-hk <kumar.hritik4w@gmail.com>
2023-05-24 01:24:33 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import "./app.css";
+import {
+  IoIosArrowDropleftCircle,
+  IoIosArrowDroprightCircle
+} from "react-icons/io";
+import sliderData from "./components/sliderData";
 
-function App() {
+export default function ImageSlider() {
+
+  function NextArrow(props) {
+    //destructuring
+  const { onClick } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick={onClick}>
+      <IoIosArrowDroprightCircle className="right-arrow" />
     </div>
   );
 }
 
-export default App;
+function PrevArrow(props) {
+  const { onClick } = props;
+  return (
+    <div onClick={onClick}>
+    <IoIosArrowDropleftCircle className="left-arrow" />
+    </div>
+  );
+}
+
+ 
+    const settings = {
+      //for dots
+      dots: true,
+      //infinite sliding
+      infinite: true,
+      //slide transition speed
+      speed: 500,
+      //slides to be shown at on a time
+      slidesToShow: 1,
+      //how mant slides to scroll at one arrowclick
+      slidesToScroll: 1,
+      //for autoplay of slides
+      autoplay: true,
+      //speed of auto-play
+      autoplaySpeed: 2000,
+      //custom next and prev arrow.. passing prop(specially onClick) to nextArrow function
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />
+
+    };
+
+    return (
+      <div className="container">
+        <h2 className="heading"> THE SLIDE SHOW</h2>
+        <Slider {...settings}>
+        {sliderData.map( (slide,index) => {
+          return (<div key={index}>
+             <img src={slide.image} alt="slide" />
+          </div>)
+        })}
+        </Slider>
+      </div>
+    );
+  
+}
+
---
+++ src/components/sliderData.js
@@ -0,0 +1,28 @@
+const sliderData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1640611219022-76b45461fec2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1561058309-8b045195c521?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1542813813-1f873f401e9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1596495578065-6e0763fa1178?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+  }
+];
+
+export default sliderData;
